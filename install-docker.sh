5f5d6dfdfed947eb63aa5a65fe1af8396dcef6e7
venkata sainath kolla <vsaikolla@gmail.com>
2025-12-28 08:16:43 -0500

    docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -9,4 +9,4 @@ dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-co
 systemctl start docker
 systemctl enable docker
 usermod -aG docker ec2-user
-echo -e "$R Logout and Login back to use docker command"
+echo -e "$R Logout and Login back to use docker command $N"
